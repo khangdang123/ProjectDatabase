ba5824b01fd3f887a2de66bef529ad1c47ef0b71
KhangD <84164518+khangdang123@users.noreply.github.com>
2023-11-19 23:33:35 -0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
+app.config['SECRET_KEY'] = 'NoteApp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
