9fe016dc4904a614c9bdeeb569642924517b7770
KhangD <84164518+khangdang123@users.noreply.github.com>
2023-11-19 22:55:12 -0800

    Create config.py

---
+++ apps/config.py
@@ -0,0 +1,13 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'NoteApp'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+app.config.from_mapping(
+        SECRET_KEY = ‘NoteApp',
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db'$
+        SQLALCHEMY_TRACK_MODIFICATIONS = False)
