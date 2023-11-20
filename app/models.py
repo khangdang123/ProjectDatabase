46772b231334e4508ea88977fa45a6c1a9442f04
KhangD <84164518+khangdang123@users.noreply.github.com>
2023-11-19 23:29:22 -0800

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from app import db, login
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
