023c6ca1239f8dba2aae02a2284e60079392077c
KhangD <84164518+khangdang123@users.noreply.github.com>
2023-11-22 17:58:36 -0800

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -26,5 +26,10 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+class CreateNote(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    note = TextAreaField('Content', validators=[DataRequired(), Length(min=1, max=500)])
+    submit = SubmitField('Save')
 
 
