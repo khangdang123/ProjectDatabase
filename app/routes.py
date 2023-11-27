d5f3fc4c1a03002c58ce6c7e68b49c42e601a7f0
KhangD <84164518+khangdang123@users.noreply.github.com>
2023-11-26 19:56:46 -0800

    Update routes.py
    fix redirect for logout

--- app/routes.py
+++ app/routes.py
@@ -34,7 +34,7 @@ def login():
 @app.route('/logout')
 def logout():
    logout_user()
-   return redirect(url_for('index'))
+   return redirect(url_for('login'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
