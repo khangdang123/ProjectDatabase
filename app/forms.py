8d9394ce61a36b02cfc6dc9b10d30ae72c6da1f0
KhangD <84164518+khangdang123@users.noreply.github.com>
2023-11-29 10:51:56 -0800

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,14 +3,14 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextA
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
-
+# User Login form for accessing their account
 class LoginForm(FlaskForm):
    username = StringField('Username', validators=[DataRequired()])
    password = PasswordField('Password', validators=[DataRequired()])
    remember_me = BooleanField('Remember Me')
    submit = SubmitField('Sign In')
 
-
+# User registration form for the new users.
 class RegistrationForm(FlaskForm):
    username = StringField('Username', validators=[DataRequired()])
    email = StringField('Email', validators=[DataRequired(), Email()])
@@ -19,30 +19,30 @@ class RegistrationForm(FlaskForm):
        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
    submit = SubmitField('Register')
 
-
+# Custom validation method for username uniqueness
    def validate_username(self, username):
        user = User.query.filter_by(username=username.data).first()
        if user is not None:
            raise ValidationError('Please use a different username.')
 
-
+# Custom validation method for email uniqueness
    def validate_email(self, email):
        user = User.query.filter_by(email=email.data).first()
        if user is not None:
            raise ValidationError('Please use a different email address.')
 
-
+# Create Note form
 class CreateNote(FlaskForm):
    title = StringField('Title', validators=[DataRequired()])
    note = TextAreaField('Content', validators=[DataRequired(), Length(min=1, max=500)])
    submit = SubmitField('Save')
 
-
+# Edit Note form
 class EditNoteForm(FlaskForm):
    title = StringField('Title', validators=[DataRequired()])
    content = TextAreaField('Content', validators=[DataRequired()])
 
-
+# Comment Note form
 class CommentForm(FlaskForm):
    text = StringField('Comment', validators=[DataRequired()])
    submit = SubmitField('Add Comment')
